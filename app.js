a6a3606e188271cb440d5d84369ca8ee1d5bbabf
Kuromaea_3 <poitevin.bastien@gmail.com>
2024-01-20 03:28:01 +0100

    commit_4

--- app.js
+++ app.js
@@ -12,5 +12,23 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
-// FUNCTION 2
+// FUNCTION 2 : GSAP
+
+gsap.registerPlugin(ScrollTrigger);
+
+const textElements = gsap.utils.toArray('.text');
+
+textElements.forEach(text => {
+    gsap.to(text, {
+        backgroundSize: '100%',
+        ease: 'none',
+        scrollTrigger: {
+            trigger: text,
+            start: 'center 80%',
+            end: 'center 20%',
+            scrub: true,
+        },
+    });
+});
+
 
